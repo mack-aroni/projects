8f8dace816880278fdf406c8a8f323d0e73f56b5
Ethan <emachleder10@stuy.edu>
2023-07-21 01:13:21 -0400

    reformatted minesweeper as a class like sudoku

--- EverythingBagel/minesweeper.py
+++ EverythingBagel/minesweeper.py
@@ -1,47 +1,86 @@
 import random as rand
 import numpy as np
 
-"""
-createBoard(num, size):
-function that creates the static board of
-size x size with num amount of mines
-"""
-def createBoard(num, size):
-    mainBoard = np.array(
-        [[0 for i in range(size)] for i in range(size)])
-    c = 0
-    while c < num:
-        x = int(rand.random()*size)
-        y = int(rand.random()*size)
-        if mainBoard[x][y] == 0:
-            mainBoard[x][y] = 1
-            c = c + 1
-    return mainBoard
+class minesweeper:
 
-"""
-printBoard(board):
-formats and prints the input board
-"""
-def printBoard(board):
-    s = "    "
-    for i in range(size):
-        if i < 9:
-            s = s + "  " + str(i+1) + " "
-        else:
-            s = s + "  " + str(i+1)
-    print(s)
-    for r in range(size):
-        if r < 9:
-            s = str(r+1) + "   "
-        else:
-            s = str(r+1) + "  "
-        for c in range(size):
-            s = s + "| " + str(board[r][c]) + " "
-        s = s + "| "
+    """
+    init(num, size):
+    creates the static board of size x size with num amount of mines
+    """
+    def __init__(self, num, size):
+        self.size = size
+        self.mainBoard = np.array(
+            [["-" for i in range(size)] for i in range(size)])
+        c = 0
+        while c < num:
+            x = int(rand.random()*size)
+            y = int(rand.random()*size)
+            if self.mainBoard[x][y] == "-":
+                self.mainBoard[x][y] = "*"
+                c = c + 1
+
+    """
+    isValid(r,c):
+    checks if the coordinates are on the grid
+    """
+    def isValid(self, x, y):
+        return x >= 0 and x < self.size and y >= 0 and y < self.size
+
+    """
+    chooseTile(x,y)
+    """
+    def chooseTile(self, x, y):
+        c = 0
+        # left cell
+        if self.isValid(x-1, y):
+            c = c + 1
+        # top left cell
+        if self.isValid(x-1, y-1):
+            c = c + 1
+        # bottom left cell
+        if self.isValid(x-1, y+1):
+            c = c + 1
+        # top cell
+        if self.isValid(x, y-1):
+            c = c + 1
+        # bottom cell
+        if self.isValid(x, y+1):
+            c = c + 1
+        # right cell
+        if self.isValid(x+1, y):
+            c = c + 1
+        # top right cell
+        if self.isValid(x+1, y-1):
+            c = c + 1
+        # bottom right cell
+        if self.isValid(x+1, y+1):
+            c = c + 1
+
+    """
+    printBoard():
+    formats and prints board
+    """
+    def printBoard(self):
+        s = "    "
+        for i in range(size):
+            if i < 9:
+                s = s + "  " + str(i+1) + " "
+            else:
+                s = s + "  " + str(i+1)
         print(s)
+        for r in range(size):
+            if r < 9:
+                s = str(r+1) + "   "
+            else:
+                s = str(r+1) + "  "
+            for c in range(size):
+                s = s + "| " + str(self.mainBoard[r][c]) + " "
+            s = s + "| "
+            print(s)
 
 if __name__ == "__main__":
-    num = 10
     size = 9
-    board = createBoard(num,size)
-    printBoard(board)
+    num = 10
+    game = minesweeper(num,size)
+    game.printBoard()
+    print(game.isValid(8,0))
