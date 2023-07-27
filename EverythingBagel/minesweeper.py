9f14f3a387ce5c187278a528b8242a276ce066d0
Ethan <emachleder10@stuy.edu>
2023-07-27 01:29:05 -0400

    added game run loop

--- EverythingBagel/minesweeper.py
+++ EverythingBagel/minesweeper.py
@@ -8,7 +8,9 @@ class minesweeper:
     creates the static board of size x size with num amount of mines
     """
     def __init__(self, num, size):
+        self.count = 0
         self.size = size
+        self.num = num
         self.mainBoard = np.array(
             [["-" for i in range(size)] for i in range(size)])
         self.gameBoard = np.array(
@@ -29,18 +31,25 @@ class minesweeper:
     def isValid(self, x, y):
         return x >= 0 and x < self.size and y >= 0 and y < self.size
 
+    """
+    hasMine(x, y):
+    returns if the tile contains a mine
+    """
     def hasMine(self, x, y):
         if self.isValid(x,y):
             return self.mainBoard[x][y] == "*"
 
     """
-    chooseTile(x,y)
+    chooseTile(x,y):
+    picks the tile at the x and y coordinate
+    if the tile is a bomb returns false
+    if 
     """
     def chooseTile(self, x, y):
         if self.mainBoard[x][y] == "*":
             #temp
             print("Gameover")
-            return False
+            return
         c = 0
         # left cell
         if self.hasMine(x-1, y):
@@ -66,14 +75,12 @@ class minesweeper:
         # bottom right cell
         if self.hasMine(x+1, y+1):
             c = c + 1
-
         # temp
-        print("Tile: "+str(x+1)+" "+str(y+1)+" count: "+str(c))
-
+        print("Tile: "+str(x+1)+","+str(y+1)+" count: "+str(c))
         # changes the value of the tile to count
         self.gameBoard[x][y] = str(c)
-
-        # if count == 0 iterate until all connecting tiles have count > 0
+        self.count = self.count + 1
+        # if count == 0 recursively iterate until all connecting tiles have count > 0
         if c == 0:
             # left cell
             if self.isValid(x-1, y):
@@ -107,7 +114,19 @@ class minesweeper:
             if self.isValid(x+1, y+1):
                 if self.gameBoard[x+1][y+1] == "?":
                     self.chooseTile(x+1,y+1)
-
+        
+    """
+    run():
+    main run loop for minesweeper game
+    """
+    def run(self):
+        game.printBoard(game.mainBoard)
+        s = input("(row,col): ")
+        while self.count == (self.size * self.size - self.num) or s != "end":
+            s = s.split(" ")
+            game.chooseTile(int(s[0])-1,int(s[1])-1)
+            game.printBoard(game.gameBoard)
+            s = input("(row,col): ")
 
     """
     printBoard():
@@ -134,13 +153,7 @@ class minesweeper:
             print(s)
 
 if __name__ == "__main__":
-    size = 9
-    num = 10
+    size = 2#9
+    num = 1#10
     game = minesweeper(num,size)
-    game.printBoard(game.mainBoard)
-    s = input("(row,col): ")
-    while s != "end":
-        s = s.split(" ")
-        game.chooseTile(int(s[0])-1,int(s[1])-1)
-        game.printBoard(game.gameBoard)
-        s = input("(row,col): ")
+    game.run()
