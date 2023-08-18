0001a9b866362976abd209714bcce06312545769
Ethan <emachleder10@stuy.edu>
2023-08-18 00:31:08 -0400

    minor changes

--- LeetCode/Easy/21_Merge_Two_Sorted_Lists.py
+++ LeetCode/Easy/21_Merge_Two_Sorted_Lists.py
@@ -2,7 +2,8 @@
 21. MERGE TWO SORTED LISTS
 You are given the heads of two sorted linked lists list1 and list2.
 
-Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+Merge the two lists into one sorted list. The list should be made by
+splicing together the nodes of the first two lists.
 
 Return the head of the merged linked list.
 """
