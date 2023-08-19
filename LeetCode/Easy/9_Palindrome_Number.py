ac0a7051913accd7f489fa5795e46a0150adce3c
Ethan <emachleder10@stuy.edu>
2023-08-19 18:40:44 -0400

    added stats and comments

--- LeetCode/Easy/9_Palindrome_Number.py
+++ LeetCode/Easy/9_Palindrome_Number.py
@@ -8,28 +8,31 @@ palindrome, and false otherwise.
 
 """
 first solution
+(54ms/49.31%)(13.5MB/6.56%)
 not using strings
 1. eliminate base cases (0, neg)
 2. use math.log10 to get magnitude of int
-3. compare ints from back to front while
-eliminating front and back ints as we go
+3. compare ints from back to front using
+
 """
 def isPalindrome(x):
     if (x == 0):
         return True
     if (x < 0):
         return False
-    f = int(math.log10(x))
-    while x > 0:
-        if (x // 10**f) != (x % 10**1):
+    f = int(math.log10(x)) #front pointer
+    b = 0 #end pointer
+    while f > b:
+        if (x // 10**f) != (x % 10**(b+1) // 10**b):
             return False
-        x = (x - (x // 10**f) * 10**f)
-        x //= 10
-        f = f - 2
+        x = x - (x // 10**f) * 10**f #remove top int of x
+        f = f - 1 
+        b = b + 1
     return True
 
 """
 second solution
+(63ms/28.85%)(13.2MB/85.59%)
 reverse int and compare
 """
 def isPalindrome_2(self, x):
@@ -38,9 +41,9 @@ def isPalindrome_2(self, x):
         reversed_num = 0
         temp = x
         while temp != 0:
-            digit = temp % 10
-            reversed_num = reversed_num * 10 + digit
-            temp //= 10
+            digit = temp % 10 #take last int
+            reversed_num = reversed_num * 10 + digit #add to front of reversed
+            temp //= 10 # remove last int
         return reversed_num == x
 
 if __name__ == "__main__":
