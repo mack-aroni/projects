972644a965cbade198520bed20a72747a173ed57
mack-aroni <etanm2013@gmail.com>
2024-05-15 18:51:51 -0400

    ported to express framework

--- WebSocketChatApp/README.md
+++ WebSocketChatApp/README.md
@@ -1,3 +1,4 @@
-npm i socket.io
 npm install
+npm i socket.io
 npm i nodemon
+npm i express
--- WebSocketChatApp/server/index.js
+++ WebSocketChatApp/server/index.js
@@ -1,9 +1,22 @@
-import { createServer } from "http"
+import express from 'express'
 import { Server } from "socket.io"
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-const myServer = createServer()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-const io = new Server(myServer, {
+const PORT = process.env.PORT || 3500
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, "public")))
+
+const expressServer = app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+})
+
+const io = new Server(expressServer, {
   cors: {
     origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500","http://127.0.0.1:5500"]
   }
@@ -16,5 +29,3 @@ io.on('connection', socket => {
     io.emit('message', `${socket.id.substring(0,5)}: ${data}`)
   })
 })
-
-myServer.listen(3500, () => {console.log("listening on port 3500")})
